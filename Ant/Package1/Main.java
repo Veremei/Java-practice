394fb7542f02dd2902d1416f98a974996a54c0e4
Daniel <33792509+Veremei@users.noreply.github.com>
2018-11-26 20:03:05 +0300

    Add files via upload

---
+++ Ant/Package1/Main.java
@@ -0,0 +1,10 @@
+
+package org.bsuir.gr673902.student.Package1;
+import org.bsuir.gr673902.student.Package1.util.PrintStr;
+
+
+public class Main {
+    public static void main(String []args){
+        PrintStr.Print(args);
+    }
+}
---
+++ Ant/Package1/util/PrintStr.java
@@ -0,0 +1,11 @@
+package org.bsuir.gr673902.student.Package1.util;
+
+
+public class PrintStr {
+    static public void Print(String []strings){
+	System.out.println("Entered text:");
+        for(int i = 0; i < strings.length; i++){
+            System.out.println(strings[i]);
+        }
+    }
+}
